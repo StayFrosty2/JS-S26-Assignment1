fdb5f85b9224929c0281ff3b79adad9c9d63a97b
Claire <cameron.liam.yon@gmail.com>
2026-06-17 14:42:00 -0400

    swapped divs to uls, added variables

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Variable to store the amount of options used to create the story
+let optionsTotal = 5;
+
+// Set of Arrays to store all of the options
+let optionArray1 = ["My Mom", "My Dad", "My Dog", "My Sibling", "The Firefighter", "My Teacher"];
+let optionArray2 = ["sat on", "talked to", "found", "worked with", "jumped over", "kissed"];
+let optionArray3 = ["a silly", "an evil", "an awesome", "a suspicious", "a giant", "a tiny"];
+let optionArray4 = ["Hedgehog", "Fox", "Cat", "Bug", "Crocodile", "Bird"];
+let optionArray5 = ["in the wall", "on the roof", "at the school", "in the soup pot", "in the garage", "on the moon"];
+
+// Get the options unordered lists
+const options1 = document.querySelector("#options1");
+const options2 = document.querySelector("#options2");
+const options3 = document.querySelector("#options3");
+const options4 = document.querySelector("#options4");
+const options5 = document.querySelector("#options5");
+
+// Loop to display all of the options in the unordered lists
+for (let i = 0; i < optionsTotal; i++) {
+    
+}
